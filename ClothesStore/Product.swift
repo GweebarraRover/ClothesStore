f9f08cb667e57300ae9dcaf2351dcb353eefa45a
Keelin <keelin21@live.ie>
2016-12-06 21:46:47 +0000

    Added base category view, storeApiManager & product objects files

---
+++ ClothesStore/Product.swift
@@ -0,0 +1,19 @@
+//
+//  Product.swift
+//  ClothesStore
+//
+//  Created by Keelin Devenney on 06/12/2016.
+//  Copyright © 2016 KeelinDevenney. All rights reserved.
+//
+
+import UIKit
+
+class Product: NSObject {
+
+    var name: String?
+    var category: String?
+    var price: Double?
+    var quantity: Int?
+    
+    //init()
+}
---
+++ ClothesStore/StoreManager.swift
@@ -0,0 +1,20 @@
+//
+//  StoreManager.swift
+//  ClothesStore
+//
+//  Created by Keelin Devenney on 06/12/2016.
+//  Copyright © 2016 KeelinDevenney. All rights reserved.
+//
+
+import UIKit
+
+class StoreManager: NSObject {
+
+    override init() {
+        super.init()
+    }
+    
+    func initialStoreAPICall() {
+        
+    }
+}
